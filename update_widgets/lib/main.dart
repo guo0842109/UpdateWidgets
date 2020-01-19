2409f569af749a2705929dd29c98d546e178d0eb
guohaibing <guohaibing@yto.net.cn>
2020-01-19 16:56:10 +0800

    no message

--- update_widgets/lib/main.dart
+++ update_widgets/lib/main.dart
@@ -1,14 +1,68 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(new MyApp());
+  runApp(MaterialApp(
+    title: '页面跳转返回数据',
+    home: FirstPage(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
+class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Image.asset('images/lakes_header.jpg'),
+    return Scaffold(
+      appBar: AppBar(title: Text('找小姐姐要电话'),),
+      body: Center(
+        child: RouteBtton(),
+      ),
+    );
+  }
+}
+
+class RouteBtton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      onPressed: (){
+        _navigateToXiaoJieJie(context);
+      },
+      child: Text('去找小姐姐'),
+    );
+  }
+}
+
+_navigateToXiaoJieJie(BuildContext context) async{//移步
+  final result = await Navigator.push(context, new MaterialPageRoute(
+      builder: (context)=>XiaoJieJie())
+  );
+
+  Scaffold.of(context).showSnackBar(SnackBar(content: Text('$result'),));
+}
+
+class XiaoJieJie extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('我是小姐姐'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            RaisedButton(
+              child: Text('大长腿小姐姐'),
+              onPressed: (){
+                Navigator.pop(context,'大长腿小姐姐：15929188889');
+              },
+            ),
+            RaisedButton(
+              child: Text('小蛮腰小姐姐'),
+              onPressed: (){
+                Navigator.pop(context,'小蛮腰小姐姐：15929100009');
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
